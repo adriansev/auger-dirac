3ad69a3e9d8bc0c3def45425d30b9adf205ba8f5
asevcenc <adrian.sevcenco@cern.ch>
2016-11-02 01:50:28 +0200

    output dir in place; to be check LFC/DFC registration of output

--- dirac.job_submit.py
+++ dirac.job_submit.py
@@ -1,5 +1,31 @@
 #!/bin/env python
 
+##################################
+###   STEERING VARIABLES
+##################################
+
+USE_DIRAC_CE_SE = 0
+
+##################################
+##################################
+
+## this corsika versions will be used from CVMFS
+corsika_version = "CORSIKA-74100_Fluka.2011.2c.2"
+corsika_bin = "corsika74100Linux_QGSII_fluka_thin"
+
+## Variable definitions
+se_dpm1    = "dpm1.egee.cesnet.cz"
+se_dirac1  = "CESNET-disk"
+
+site_lcg = "prague_cesnet_lcg2"
+site_dirac = "LCG.CESNET.cz"
+
+ce1 = "cream1.grid.cesnet.cz"
+ce2 = "cream2.grid.cesnet.cz"
+ce3 = "cream1.farm.particle.cz"
+
+########################################################################################
+########################################################################################
 import os,re,sys,argparse
 
 from DIRAC.Core.Base import Script
@@ -7,6 +33,7 @@ Script.parseCommandLine()
 
 from DIRAC.Interfaces.API.Job import Job
 from DIRAC.Interfaces.API.Dirac import Dirac
+
 
 def get_filepaths(directory):
     file_paths = []  # List which will store all of the full filepaths.
@@ -24,42 +51,65 @@ def get_filepaths(directory):
 ## path="simulation_run_test_tag_XYZ_v3"
 
 if (len(sys.argv) < 2) :
-    print 'the input directory should be specified'
+    print ('the input directory should be specified')
     sys.exit(os.EX_USAGE)
 
 path = sys.argv[1]
-print "Input directory: '{0}'".format(path)
+print ("Input directory: '{0}'".format(path))
+##############################################################
 
 full_file_paths = get_filepaths(path)
 input_files = full_file_paths
 
-## Variable definitions
-se = "dpm1.egee.cesnet.cz"
-ce1 = "cream1.grid.cesnet.cz"
-ce2 = "cream2.grid.cesnet.cz"
-ce3 = "cream1.farm.particle.cz"
-site = "prague_cesnet_lcg2"
-se_dirac = "CESNET-disk"
-site_dirac = "LCG.CESNET.cz"
-corsika_version = "CORSIKA-74100_Fluka.2011.2c.2"
-corsika_bin = "corsika74100Linux_QGSII_fluka_thin"
 
 ## root dir for job output storage
-grid_basedir_output = "/grid/auger/user/asevcenc/"
+base_dir_dirac = "/auger/user/a/asevcenc/"
+base_dir_lfc = "/grid/auger/user/asevcenc/"
+
+grid_basedir_output = ""
+site = ""
+se = ""
+
+if ( USE_DIRAC_CE_SE == 1 ) :
+    grid_basedir_output = base_dir_dirac
+    site = site_dirac
+    se = se_dirac1
+else :
+    grid_basedir_output = base_dir_lfc
+    se = se_dpm1
+    site = site_lcg
+
 
 ##  MAIN LOOP OVER ALL INPUT FILES
 for input_file in input_files:
-    print "Input file is : '{0}'".format(input_file)
+#    print "Input file is : {}".format(input_file)
+    print '\nInput file is : ', input_file
 
     ## GET RUN NUMBER FROM INPUT FILE
     f = open(input_file, 'r')
     runnr = -1
+    e_min = -1
+    e_max = -1
+    theta_min = -1
+    theta_max = -1
+    prmpar = -999
 
+    nr_line = 0
     for line in f:
-            line = line.strip()
-            columns = line.split()
+        nr_line += 1
+        line = line.strip()
+        columns = line.split()
+        if ( columns[0] == "RUNNR" ) :
             runnr = columns[1]
-            break
+        if ( columns[0] == "PRMPAR" ) :
+            prmpar = columns[1]
+        if ( columns[0] == "ERANGE" ) :
+            e_min = columns[1]
+            e_max = columns[2]
+        if ( columns[0] == "THETAP" ) :
+            theta_min = columns[1]
+            theta_max = columns[2]
+
 
     f.close()
 
@@ -68,6 +118,11 @@ for input_file in input_files:
         break
 
     print "runnr = '{0}'".format(runnr)
+    print "prmpar = '{0}'".format(prmpar)
+    print "Energy Min = '{0}'".format(e_min)
+    print "Energy Max = '{0}'".format(e_max)
+    print "Theta Min = '{0}'".format(theta_min)
+    print "Theta Max = '{0}'".format(theta_max)
 
     ######################################
     ##     BEGIN JOB DESCRIPTION
@@ -82,44 +137,43 @@ for input_file in input_files:
     run_log = input_file_base + ".log"
     dat = input_file_base.replace('aug', 'DAT')
     datlong = dat + ".long"
-    output_files= [run_log, 'fluka11.out', 'fluka15.err', dat, datlong ]
+    output_files = [run_log, 'fluka11.out', 'fluka15.err', dat, datlong ]
+    print output_files
 
     ## prepare the output location in GRID storage; the input path will be the used also for GRID storage
-    outdir = grid_basedir_output + path + "/" + str(runnr)
+    # outdir = grid_basedir_output + path + "/" + str(e_min) + "_" + str(e_max) + "/" + str(theta_min) + "_" + str(theta_max) + "/" + str(prmpar) + "/" + str(runnr)
+    outdir = path + "/" + str(e_min) + "_" + str(e_max) + "/" + str(theta_min) + "_" + str(theta_max) + "/" + str(prmpar) + "/" + str(runnr)
+    print outdir
 
-    ## ALWAYS, INFO, VERBOSE, WARN, DEBUG
+    ### ALWAYS, INFO, VERBOSE, WARN, DEBUG
     j.setLogLevel('debug')
 
-    j.setName('AUGER test simulation')
-
+    #j.setDestination(site)
     j.setDestinationCE(ce1)
-    ##    j.setDestination(site_dirac)
-
+    j.setName('AUGER test simulation')
     j.setCPUTime(345600) ## 4 days
 
-    ## download the script for preparing corsika input file for usage with cvmfs
-##    j.setExecutable( 'curl', arguments = ' -fsSLkO http://issaf.spacescience.ro/adrian/AUGER/make_run4cvmfs',logFile='cmd_logs.log')
-##    j.setExecutable( 'chmod', arguments = ' +x make_run4cvmfs',logFile='cmd_logs.log')
+    ### download the script for preparing corsika input file for usage with cvmfs
+###    j.setExecutable( 'curl', arguments = ' -fsSLkO http://issaf.spacescience.ro/adrian/AUGER/make_run4cvmfs',logFile='cmd_logs.log')
+###    j.setExecutable( 'chmod', arguments = ' +x make_run4cvmfs',logFile='cmd_logs.log')
 
-    ## create the simulation script configured for use with cvmfs
-    ## set the make_run4cvmfs arguments to include the corsika_version and corsika_bin
+    ### create the simulation script configured for use with cvmfs
+    ### set the make_run4cvmfs arguments to include the corsika_version and corsika_bin
     make_run4cvmfs_arg = input_file_base + " " + corsika_version + " " + corsika_bin
     j.setExecutable( '/cvmfs/auger.egi.eu/utils/make_run4cvmfs', arguments = make_run4cvmfs_arg, logFile='cmd_logs.log')
 
-    ## run simulation
+    ### run simulation
     j.setExecutable( './execsim',logFile='cmd_logs.log')
 
-    j.setOutputSandbox(output_files)
+    ###j.setOutputSandbox(output_files)
     j.setOutputData(output_files, outputSE=se, outputPath=outdir)
 
-    ##j.runLocal()  ## test local
+    #j.runLocal()  ## test local
 
     jobID = dirac.submit(j)
-    print 'Submission Result: ',jobID
-
     id = str(jobID) + "\n"
-
+    print 'Submission Result: ',jobID
     with open('jobids.list', 'a') as f_id_log:
-        f_id_log.write(jobID.Value + '\n')
+        f_id_log.write(id + '\n')
 
 
