c39281436987ee262dfe2d878fc774121da68570
asevcenc <adrian.sevcenco@cern.ch>
2016-12-27 17:31:58 +0200

    dirac.job_status.py :: add summary, debug output and job logging

--- dirac.job_status.py
+++ dirac.job_status.py
@@ -10,5 +10,12 @@ from DIRAC.Interfaces.API.Dirac import Dirac
 
 dirac = Dirac()
 jobid = sys.argv[1]
+
 print dirac.status(jobid)
+
+dirac.getJobSummary(jobid, outputFile=None, printOutput=True)
+
+print dirac.getJobDebugOutput(jobid)
+
+print dirac.getJobLoggingInfo(jobid, printOutput=False)
 
